204374677901562a558f3fedd9cc0f3de17d13e9
Lukas Dullinger <ldulling@c4r4p1.42vienna.com>
2023-10-18 13:08:11 +0200

    fixed conditions for print_prefix

--- print_nbr.c
+++ print_nbr.c
@@ -1,7 +1,7 @@
 #include "ft_printf.h"
 
 static int	nbrlen(long nbr, t_format *format);
-static int 	totallen(long nbr, int len_nbr, t_format *format);
+static int 	totalnbrlen(long nbr, int len_nbr, t_format *format);
 static int	print_prefix(long nbr, t_format *format);
 static int	print_nbr_in_correct_base(long nbr, t_format *format);
 
@@ -9,7 +9,7 @@ int	print_nbr(long nbr, t_format *format)
 {
 	char	padding;
 	int		len_nbr;
-	int		len_total;
+	int		len_totalnbr;
 	int		printed_count;
 
 	if (format -> zero && !format -> minus && format -> precision == -1) // Possibly parentheses needed
@@ -17,20 +17,20 @@ int	print_nbr(long nbr, t_format *format)
 	else
 		padding = ' ';
 	len_nbr = nbrlen(nbr, format);
-	len_total = totallen(nbr, len_nbr, format);
+	len_totalnbr = totalnbrlen(nbr, len_nbr, format);
 	printed_count = 0;
-	if (format -> zero)
+	if (padding == '0')
 		printed_count += print_prefix(nbr, format);
-	if (!format -> minus && len_total < format -> width)
-		printed_count += ft_putnchar_fd(padding, format -> width - len_total, 1);
-	if (!format -> zero && format -> specifier != 'u' && !(nbr == 0 && format -> precision == 0))
+	if (!format -> minus && len_totalnbr < format -> width)
+		printed_count += ft_putnchar_fd(padding, format -> width - len_totalnbr, 1);
+	if (padding == ' ' && format -> specifier != 'u' && !(nbr == 0 && format -> precision == 0))
 		printed_count += print_prefix(nbr, format);
 	if (format -> precision > len_nbr)
 		printed_count += ft_putnchar_fd('0', format -> precision - len_nbr, 1);
 	if (!(nbr == 0 && format -> precision == 0))
 		printed_count += print_nbr_in_correct_base(nbr, format);
-	if (format -> minus && len_total < format -> width)
-		printed_count += ft_putnchar_fd(' ', format -> width - len_total, 1);
+	if (format -> minus && len_totalnbr < format -> width)
+		printed_count += ft_putnchar_fd(' ', format -> width - len_totalnbr, 1);
 	return (printed_count);
 }
 
@@ -57,21 +57,21 @@ static int	nbrlen(long nbr, t_format *format)
 	return (len_nbr);
 }
 
-static int 	totallen(long nbr, int len_nbr, t_format *format)
+static int 	totalnbrlen(long nbr, int len_nbr, t_format *format)
 {
-	int	len_total;
+	int	len_totalnbr;
 
-	len_total = len_nbr;
+	len_totalnbr = len_nbr;
 	if (len_nbr < format -> precision)
-		len_total = format -> precision;
+		len_totalnbr = format -> precision;
 	if (format -> specifier != 'u' && format -> specifier != 'x' && format -> specifier != 'X')
 		if (!(nbr == 0 && format -> precision == 0))
 			if (nbr < 0 || format -> plus || format -> space)
-				len_total++;
+				len_totalnbr++;
 	if (format -> specifier == 'x' || format -> specifier == 'X')
 		if (format -> hash)
-			len_total += 2;
-	return (len_total);
+			len_totalnbr += 2;
+	return (len_totalnbr);
 }
 
 static int	print_prefix(long nbr, t_format *format)
